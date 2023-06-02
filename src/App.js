5b1c2dd78713b663be03918688ee20a5c6d553a0
Priyadarsini Biswal <priyankabiswal368@gmail.com>
2023-06-02 10:45:09 +0530

    profile page added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
    <BrowserRouter>
    <Routes>
     <Route path="/" element={<Homepage/>} />
-    {/* <Route path="profile" element={<Profile/>} /> */}
+    <Route path="profile" element={<Profile/>} />
    </Routes>
    </BrowserRouter>
     </div>
